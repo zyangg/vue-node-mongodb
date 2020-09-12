cdd7e91f07a87b2f80ad76cf1512a5ea0953bbca
zyangg <1511191171@qq.com>
2020-09-12 20:57:14 +0800

    third

--- serve/models/Hero.js
+++ serve/models/Hero.js
@@ -2,6 +2,32 @@ const mongoose = require('mongoose')
 
 const scheme = new mongoose.Schema({
     name: { type: String },
-    icon: String
+    icon: String,
+    title: String,
+    categories: [{ type: mongoose.SchemaTypes.ObjectId, ref: 'Category' }],
+    scores: {
+        difficult: Number,
+        skills: Number,
+        attack: Number,
+        survive: Number
+    },
+    skills: [{
+        icon: { type: String },
+        name: String,
+        description: String,
+        tips: String
+    }],
+   
+     items1: [{ type: mongoose.SchemaTypes.ObjectId, ref: 'Item' }],
+    items2: [{ type: mongoose.SchemaTypes.ObjectId, ref: 'Item' }],
+    usage_tips: String,
+    battleTips: String,
+    teamTips: String,
+    parents: [{
+        hero: { type: mongoose.SchemaTypes.ObjectId, ref: 'Hero' },
+        descriptin: String
+    }]
+    
+
 })
 module.exports = mongoose.model('Hero', scheme)
